f8487ced2050d6bf1806f1460d8021c1df6ba269
Mariam_Taher <mariamtaher4444@gmail.com>
2023-09-19 10:26:23 +0300

    App Initialization

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:app_2/Layout/home_layout.dart';
+import 'package:app_2/core/theme/application_theme.dart';
+import 'package:app_2/modules/Splash_Screen/splash_screen.dart';
+
+void main() {
+  runApp(const MyApplication());
+}
+
+class MyApplication extends StatelessWidget {
+  const MyApplication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ApplicationTheme.lightTheme,
+      darkTheme: ApplicationTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      // give it key of screen to start with it
+      initialRoute: SplashScreen.routeName,
+      // map faster the array on search with key
+      routes: {
+        // call back function ( function in another place)
+        SplashScreen.routeName: (context) => const SplashScreen(),
+        HomeLayout.routeName: (context) => HomeLayout(),
+      },
+    );
+  }
+}
---
+++ lib/modules/quran/quran.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Quran extends StatelessWidget {
+  const Quran({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset("assets/images/picture.png"),
+
+      ],
+    );
+  }
+}
